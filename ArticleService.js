fe335b4b6f03ddc7e911db82b5c95240b2c16d90
Kang siyeon <rkdtldus0115@naver.com>
2025-05-02 11:19:10 +0900

    .then(),.catch() 추가

--- ArticleService.js
+++ ArticleService.js
@@ -1,49 +1,73 @@
 const BASE_URL = "https://panda-market-api-crud.vercel.app/articles";
 
-
 export async function getArticleList({ page, pageSize, keyword }) {
   const query = new URLSearchParams({ page, pageSize, keyword }).toString();
-  const res = await fetch(`${BASE_URL}?${query}`, {
-    method : 'GET'
-  });
-  return await res.json();
+  try {
+    const res = await fetch(`${BASE_URL}?${query}`, { method: 'GET' });
+    if (!res.ok) {
+      throw new Error('API 에러');
+    }
+    return await res.json();
+  } catch (error) {
+    console.error('getArticleList 에러:', error);
+  }
 }
-
 
 export async function getArticle(id) {
-  const res = await fetch(`${BASE_URL}/${id}`, {
-    method : 'GET'
-  });
-  return await res.json();
+  try {
+    const res = await fetch(`${BASE_URL}/${id}`, { method: 'GET' });
+    if (!res.ok) {
+      throw new Error('API 에러');
+    }
+    return await res.json();
+  } catch (error) {
+    console.error('getArticle 에러:', error);
+  }
 }
-
 
 export async function createArticle({ title, content, image }) {
-  const res = await fetch(BASE_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ title, content, image }),
-  });
-  return await res.json();
+  try {
+    const res = await fetch(BASE_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, content, image }),
+    });
+    if (!res.ok) {
+      throw new Error('API 에러');
+    }
+    return await res.json();
+  } catch (error) {
+    console.error('createArticle 에러:', error);
+  }
 }
-
 
 export async function patchArticle(id) {
-  const res = await fetch(`${BASE_URL}/${id}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  return await res.json();
+  try {
+    const res = await fetch(`${BASE_URL}/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!res.ok) {
+      throw new Error('API 에러');
+    }
+    return await res.json();
+  } catch (error) {
+    console.error('patchArticle 에러:', error);
+  }
 }
 
-
 export async function deleteArticle(id) {
-  const res = await fetch(`${BASE_URL}/${id}`, {
-    method: "DELETE",
-  });
-  return await res.json();
+  try {
+    const res = await fetch(`${BASE_URL}/${id}`, { method: "DELETE" });
+    if (!res.ok) {
+      throw new Error('API 에러');
+    }
+    return await res.json();
+  } catch (error) {
+    console.error('deleteArticle 에러:', error);
+  }
 }
