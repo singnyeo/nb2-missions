3cf43cbeda2abff2e67a61a5d1cd0c48969f7774
Kang si yeon <rkdtldus0115@naver.com>
2025-05-23 16:24:53 +0900

    상품 상세 조회 API

--- 02-secondHandMarket/routes/products.js
+++ 02-secondHandMarket/routes/products.js
@@ -5,6 +5,7 @@ const { CreateDto } = require('../dtos/product.dto');
 
 var router = express.Router();
 
+//상품 등록 API
 router.post ('/create', async (req, res, next) => {
   assert(req.body, CreateDto)
 
@@ -16,4 +17,9 @@ router.post ('/create', async (req, res, next) => {
   res.json({ id : product.id })
 });
 
+//상품 상세 조회 API
+router.get('/list', async (req, res, next) => {
+  const products = await db.product.findMany();
+  res.json(products);
+});
 module.exports = router;
