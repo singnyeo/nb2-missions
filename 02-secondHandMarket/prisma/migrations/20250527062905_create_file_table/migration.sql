c485c476cc19f2d93432731dd3c74c3fd97be480
Kang si yeon <rkdtldus0115@naver.com>
2025-05-27 15:29:52 +0900

    업로드 API 세팅

---
+++ 02-secondHandMarket/prisma/migrations/20250527062905_create_file_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "File" (
+    "id" SERIAL NOT NULL,
+    "fileName" TEXT NOT NULL,
+    "mimeType" TEXT NOT NULL,
+    "size" INTEGER NOT NULL,
+    "url" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "File_pkey" PRIMARY KEY ("id")
+);
