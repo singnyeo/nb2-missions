88adbce28c6c330f6d7fe4d5418ce50659a537e6
Kang si yeon <rkdtldus0115@naver.com>
2025-05-28 13:39:18 +0900

    환경 변수 설정

--- 02-secondHandMarket/app.js
+++ 02-secondHandMarket/app.js
@@ -30,4 +30,9 @@ app.use((req, res, next) => {
 
 app.use(errorHandler);
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 module.exports = app;
